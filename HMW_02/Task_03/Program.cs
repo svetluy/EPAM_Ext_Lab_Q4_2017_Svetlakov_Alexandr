13ae6e3e3cdece23e205e171f977fed3f576594e
svetluy <mikitn2009@yandex.ru>
2017-11-29 17:11:46 +0400

    Task 1,2,3

--- HMW_02/Task_03/Program.cs
+++ HMW_02/Task_03/Program.cs
@@ -1,15 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿#region Описание
+// Написать программу, которая запрашивает с клавиатуры число N и
+// выводит на экран «изображение», состоящее из N строк.
+#endregion
 namespace Task_03
 {
-    class Program
+    using static System.Console;
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            ForegroundColor = System.ConsoleColor.DarkCyan;
+
+            int n;
+            bool condition = true;
+            while (condition)
+            {
+                int i, j;
+
+                do
+                {
+                    WriteLine("Enter N");
+                    int.TryParse(ReadLine(), out n);
+                }
+                while (n <= 0);
+
+                for (i = 0; i < n; i++)
+                {
+                    for (j = 0; j <= (n - i); j++)
+                    {
+                        Write(" ");
+                    }
+                    for (j = 0; j < 2*i+1; j++)
+                    {
+                        Write("*");
+                    }
+                    WriteLine();
+                }
+
+                WriteLine("Would you like to continue?\n y    - Yes\n else - NO");
+                condition = ReadLine() == "y" ? true : false;
+            }
         }
     }
 }

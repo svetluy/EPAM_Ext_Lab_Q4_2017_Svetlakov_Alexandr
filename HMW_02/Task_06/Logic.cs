095fd5b2fb372fd47f858b664b1e280be558bfc6
svetluy <mikitn2009@yandex.ru>
2017-11-29 14:46:27 +0400

    Task5, Task6 complete

---
+++ HMW_02/Task_06/Logic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Task_06
+{
+    public static class Logic
+    {
+        public static void IsInList(int counter, string parametr, List<string> param)
+        {
+            bool isIt;
+            if (counter % 2 == 0)
+            {
+                isIt = false;
+            }
+            else
+            {
+                isIt = true;
+            }
+            CheckParams(parametr, param, isIt);
+        }
+
+        public static void CheckParams(string parametr, List<string> param, bool isIt)
+        {
+            if (isIt)
+                param.Add(parametr);
+            else
+                param.Remove(parametr);
+        }
+    }
+}
--- HMW_02/Task_06/Program.cs
+++ HMW_02/Task_06/Program.cs
@@ -1,8 +1,7 @@
-﻿using static System.Console;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Console;
+using static Task_06.Logic;
 
 namespace Task_06
 {
@@ -10,7 +9,47 @@ namespace Task_06
     {
         static void Main(string[] args)
         {
+            ForegroundColor = ConsoleColor.DarkCyan;
+            ConsoleKeyInfo key;
 
+            int boldKeyCounter=0, italicKeyCounter=0, underlineKeyCounter=0;
+            List<string> param = new List<string>();
+
+            do
+            {
+                CheckParams("None",param,param.Count==0);
+
+                Write("Параметры надписи : ");
+                foreach (var p in param)
+                {
+                    Write($"{p} ");
+                }
+                WriteLine();
+
+                WriteLine("Введите:\n\t1: bold\n\t2: italic\n\t3: underline");
+                key = ReadKey();
+                if (key.Key == ConsoleKey.D1)
+                {
+                    boldKeyCounter++;
+                    IsInList(boldKeyCounter,"Bold", param);
+                }
+                else if (key.Key == ConsoleKey.D2)
+                {
+                    italicKeyCounter++;
+                    IsInList(italicKeyCounter, "Italic", param);
+                }
+                else if (key.Key == ConsoleKey.D3)
+                {
+                    underlineKeyCounter++;
+                    IsInList(underlineKeyCounter, "Underline", param);
+                }
+                else
+                {
+                    WriteLine("\n Неизвестный параметр");
+                }
+
+                WriteLine();
+            } while (true);
         }
     }
 }

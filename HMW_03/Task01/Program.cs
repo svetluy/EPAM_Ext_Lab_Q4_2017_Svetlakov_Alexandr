26c96587124981c579f4694be78dbae3c1cd408d
svetluy <mikitn2009@yandex.ru>
2017-12-02 14:36:13 +0400

    Task 1 complete

---
+++ HMW_03/Task01/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task01
+{
+    using static System.Console;
+    using static Logic;
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.ConsoleKeyInfo key;
+            do
+            {
+                var arr = CreateArr(InputData("Input array length, n > 0 "), InputData("Input max possible value in array, value > 0 "));
+
+                WriteArr(arr);
+
+                SortArr(arr);
+
+                WriteArr(arr);
+
+                WriteLine($"Max Value in Arr is {arr[arr.Length - 1]}\nMin Value in Arr is {arr[0]}");
+
+                WriteLine("Would you like to continue?\n y - Yes\n else - No");
+
+                key = ReadKey();
+
+                WriteLine();
+            }
+            while (key.Key == System.ConsoleKey.Y);
+        } 
+    }
+}

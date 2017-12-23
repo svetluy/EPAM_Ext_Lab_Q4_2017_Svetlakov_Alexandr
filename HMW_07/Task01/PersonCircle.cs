adf9e5425eadfde7f08ef7eb53d6be66b5195e36
svetluy <mikitn2009@yandex.ru>
2017-12-23 14:58:13 +0400

    task1

---
+++ HMW_07/Task01/PersonCircle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace Task01
+{
+    public class PersonCircle
+    {
+        List<int> persons;
+
+        public PersonCircle(int n)
+        {
+            persons = CreatePersons(n);
+        }
+
+        private List<int> CreatePersons(int n)
+        {
+            List<int> persons = new List<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                persons.Add(i);
+            }
+            return persons;
+        }
+
+        public int RemovePersons()
+        {
+            do
+            {
+                for (int i = 0; i < this.persons.Count ; i++)
+                {
+                    if((i+1) % 2 == 0)
+                    {
+                        this.persons.RemoveAt(i);
+                    }
+                }
+            }
+            while (this.persons.Count != 1);
+            return this.persons[0];
+        }
+    }
+}
---
+++ HMW_07/Task01/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task01
+{
+    using System;
+    using static System.Console;
+
+    class Program
+    {
+        static void Main(string[] args) // 
+        {
+            ConsoleKeyInfo key;
+            do
+            {
+                WriteLine("Enter number of persons");
+                int.TryParse(ReadLine(), out int n);
+
+                PersonCircle personCircle = new PersonCircle(n);
+                int surviverNumber = personCircle.RemovePersons(); 
+                WriteLine($"Surviver person number is {surviverNumber}");
+                WriteLine("Would you like to continue?");
+                key = ReadKey();
+            }
+            while (key.Key == ConsoleKey.Y);
+        }
+    }
+}

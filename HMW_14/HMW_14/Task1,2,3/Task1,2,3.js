8e94e9cc4f12a6f3936aacba9178ad3f416cddc0
svetluy <mikitn2009@yandex.ru>
2018-03-24 01:07:56 +0400

    HMW_14

---
+++ HMW_14/HMW_14/Task1,2,3/Task1,2,3.js
@@ -0,0 +1,92 @@
+﻿function calc() {
+    delSimLetters(document.getElementById("calc").value);
+}
+
+function operation() {
+    doOperation(document.getElementById("operation").value);
+}
+
+function delSimLetters(text) {
+    var separators = [' ', '\t', '?', '!', ':', ';', ',', '.'];
+    var str = text + "";
+    var words;
+    var result = str;
+    var tempChar = separators[0];
+    for (var i = 0; i < separators.length; i++) {
+        str = str.split(separators[i]).join(tempChar);
+    }
+    words = str.split(tempChar);
+
+    for (i = 0; i < words.length; i++) {
+        for (var j = 0; j < words[i].length; j++) {
+            for (var k = j + 1; k < words[i].length; k++) {
+                if ((words[i][j] === words[i][k]) && (words[i].length > 1)) {
+                    result = result.split(words[i][j]).join('');
+                }
+            }
+        }
+    }
+    document.getElementById("result1").innerHTML = result;
+
+}
+
+
+function doOperation(text) {
+    var str = text + '';
+    str = str.split('=')[0];
+    text = str;
+    var separators = ['+', '-', '*', '/'];
+    var numbers;
+    var tempChar = separators[0];
+    for (var i = 0; i < separators.length; i++) {
+        str = str.split(separators[i]).join(tempChar);
+    }
+    numbers = str.split(tempChar);
+    var op;
+    var result = numbers[0];
+    for (var i = 0; i < numbers.length-1; i++) {
+        text = findSubstr(text);
+        result = choseOp(result, numbers[i+1], op);
+    }
+
+    function findSubstr(text) {
+        text = text + "";
+        for (var j = 0; j < text.length; j++) {
+            if (text[j] == '+' || text[j] == '-' || text[j] == '*' || text[j] == '/') {
+               op = text[j];
+               return text = text.substring(j+1, text.length);
+            }
+        }
+    }
+
+    document.getElementById("result2").innerHTML = result;
+}
+
+function choseOp(number1, number2, operation) {
+    switch (operation) {
+        case '+': return +number1 + +number2; 
+        case '-': return +number1 - +number2; 
+        case '*': return +number1 * +number2; 
+        case '/': return +number1 / +number2; 
+        default: return +number1; 
+    }
+}
+
+function MyMoveItem() {
+    var selected = $('.possible option:selected');
+    selected.appendTo('.wishlist');
+}
+
+function MoveAllItem() {
+    var selected = $('.possible option');
+    selected.appendTo('.wishlist');
+}
+
+function RemoveItem() {
+    var selected = $('.wishlist option:selected');
+    selected.appendTo('.possible');
+}
+function RemoveAllItem() {
+    var selected = $('.wishlist option');
+    selected.appendTo('.possible');
+}
---
+++ HMW_14/HMW_14/Task4/Task4.js
@@ -0,0 +1,47 @@
+﻿var ticks;
+var page;
+var timeout = setTimeout(letsGo, 10);
+var counter = 0;
+
+function startTimer(nextPage) {
+    ticks = 3 * 100;
+    page = nextPage;
+    timeout();
+}
+
+function letsGo() {
+    if (ticks < 1) {
+        if (typeof page == "undefined") {
+            if (confirm("Начать сначала?")) {
+                page = 'TestPage1.html'
+            }
+            else {
+                return;
+            }
+        }
+        location.assign(page);
+        return;
+    }
+
+    document.getElementById("tick").innerHTML = ticks / 100;
+    timeout = setTimeout(letsGo, 10);
+    ticks--;
+}
+
+function onPause() {
+    counter++;
+    if (counter%2 == 1) {
+        clearTimeout(timeout); 
+    }
+    else if (counter % 2 == 0) {
+         timeout = setTimeout(letsGo, 10);
+    }
+}
+
+function nextPage(page) {
+    location.assign(page);
+}
+
+function previusPage() {
+    history.back();
+}
